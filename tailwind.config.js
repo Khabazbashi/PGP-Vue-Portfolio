aa22e8001525f8622ff454e95f6927b15222a963
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-09-06 15:16:27 +0200

    Add tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      animation: {
+        ping: "ping 1s infinite",
+      },
+      keyframes: {
+        ping: {
+          "75%, 100%": { opacity: "3" },
+        },
+      },
+    },
   },
   plugins: [],
 };
