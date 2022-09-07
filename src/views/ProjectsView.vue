cd57c52a409a3a7a1aec1a3ef1728145632596a5
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-09-07 14:47:38 +0200

    Add a view for listing projects

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Section from "../components/MainSections.vue";
+</script>
+
+<template>
+  <Section
+    title="PROJECTS"
+    contentType="project"
+    :contents="[
+      {
+        subtitle: 'Title',
+        link: 'link',
+        description:
+          'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto.',
+      },
+      {
+        subtitle: 'Title',
+        link: 'link',
+        description:
+          'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. ',
+      },
+    ]"
+  />
+</template>
+
+<style scoped></style>
