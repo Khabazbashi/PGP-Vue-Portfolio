a2d7fcfe44e511512fb0e6d04458d543a5c11ebe
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-09-17 11:39:21 +0200

    Add new subsection and section

---
+++ src/components/Section.vue
@@ -0,0 +1,40 @@
+<script setup>
+import subsection from "./Subsection.vue";
+defineProps({
+  main: {
+    type: Object,
+  },
+  content: {
+    type: Array,
+  },
+});
+</script>
+
+<template>
+  <div class="main flex grow flex-col">
+    <div v-if="main.title" class="main__header flex flex-col text-center">
+      <h1 class="main__title text-5xl mt-36 mb-10 mx-9">{{ main.title }}</h1>
+      <p class="main__info text-lg mx-9 mb-32">{{ main.info }}</p>
+    </div>
+
+    <div class="main__body bg-white">
+      <subsection
+        class="mx-9"
+        v-for="(item, index) in content"
+        v-bind:key="index"
+        v-bind:data="item"
+      ></subsection>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main__info {
+  font-family: "Miriam Libre", sans-serif;
+}
+.main__title {
+  font-family: "Dosis", sans-serif;
+}
+@media (min-width: 1024px) {
+}
+</style>
---
+++ src/components/Subsection.vue
@@ -0,0 +1,66 @@
+<script>
+export default {
+  name: "ToggleDiv",
+  data: function () {
+    return {
+      isShow: false,
+    };
+  },
+
+  props: {
+    data: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="subsection first:mt-20 last:mb-20 my-9">
+    <h3 class="text-xl font-semibold">{{ data.subtitle }}</h3>
+
+    <div v-if="data.date">
+      <p class="italic">{{ data.date }}</p>
+    </div>
+
+    <div v-if="data.description">
+      <p class="mt-2 pb-7">{{ data.description }}</p>
+    </div>
+
+    <div v-if="data.link" class="flex place-content-end pr-3">
+      <a target="_blank" :href="data.link"
+        ><span class="material-symbols-outlined relative absolute">
+          link
+        </span></a
+      >
+    </div>
+
+    <div v-if="data.images" class="grid grid-cols-3 gap-3 mt-4 mb-10">
+      <div
+        class="img__container"
+        v-for="(ts, index) in data.images"
+        v-bind:key="index"
+      >
+        <img :src="ts" />
+      </div>
+    </div>
+
+    <div class="flex flex-col" v-if="data.bullets">
+      <button class="w-10 mx-auto mt-5 mb-10" @click="isShow = !isShow">
+        <img alt="Expand" src="@/assets/expand.png" />
+      </button>
+      <div v-if="isShow">
+        <ul v-for="(tst, index) in data.bullets" :key="index">
+          <li class="text-center">{{ tst }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.img__container {
+  box-shadow: 0 20px 50px rgba(0, 0, 0, 0.5);
+}
+
+@media (min-width: 1024px) {
+}
+</style>
