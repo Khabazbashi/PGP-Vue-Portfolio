69941192720e668b0066de27d6b82e12c57293e0
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-09-17 11:40:24 +0200

    Add navbar component to app

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-  <nav id="tre" class="border-b-2 text-center text-xl align-text-bottom">
-    <RouterLink class="mt-6 mb-6" to="/">Home</RouterLink>
-    <RouterLink class="mt-6 mb-6" to="/about">About</RouterLink>
-    <RouterLink class="mt-6 mb-6" to="/projects">Projects</RouterLink>
-    <RouterLink class="mt-6 mb-6" to="/techstack">Stack</RouterLink>
-  </nav>
-  <RouterView />
+  <navbar class="" />
+  <RouterView id="app-main" class="flex flex-col" />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  width: 100%;
-}
-#tre {
-  border-bottom: 1px solid rgb(53, 53, 53);
-}
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
 @media (min-width: 1024px) {
   header {
     display: flex;
