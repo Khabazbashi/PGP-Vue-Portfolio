84f1889b796b0e2e1f530b3f410df4c41fb7ca98
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-09-21 13:11:17 +0200

    reduce spacing between subsections

--- src/components/Subsection.vue
+++ src/components/Subsection.vue
@@ -47,10 +47,10 @@ export default {
     </div>
 
     <div class="flex flex-col" v-if="data.bullets">
-      <button class="w-10 mx-auto mt-5 mb-10" @click="isShow = !isShow">
+      <button class="w-10 mx-auto" @click="isShow = !isShow">
         <img alt="Expand" src="../assets/expand.png" />
       </button>
-      <div v-if="isShow">
+      <div class="p-5" v-if="isShow">
         <ul v-for="(tst, index) in data.bullets" :key="index">
           <li class="text-center">{{ tst }}</li>
         </ul>
