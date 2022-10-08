5454c228c5db710a93f5c31bab31833ff6cfed20
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-10-08 10:23:32 +0200

    Install package Values and compute shades for bg color of button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,13 +1,29 @@
+<script setup>
+import Values from "values.js";
+</script>
+
 <script>
 export default {
   props: {
     data: String,
+    color: String,
+  },
+  computed: {
+    newColor() {
+      const currentColor = new Values(this.color);
+      const shades = currentColor.shades(23);
+      var shade = "#" + shades[Math.floor(Math.random() * shades.length)].hex;
+      return shade;
+    },
   },
 };
 </script>
 
 <template>
-  <div class="flex flex-col items-center bg-blue-500 p-3">
+  <div
+    :style="{ background: newColor }"
+    class="flex flex-col items-center py-3 px-1"
+  >
     <p class="text-center text-md md:text-lg">{{ data }}</p>
   </div>
 </template>
